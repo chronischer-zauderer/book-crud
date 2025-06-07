83e3ce30497343dc6a2509d5ee8798016539c9f2
chronischer-zauderer <cardona.julio@correounivalle.edu.co>
2025-06-07 12:07:04 -0500

    fix

--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BooksService } from './books.service';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('BooksService', () => {
   let service: BooksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BooksService],
+      providers: [
+        BooksService,
+        {
+          provide: getModelToken('Book'),
+          useValue: {}, // Puedes poner un mock simple o un mock de mongoose model aquí
+        },
+      ],
     }).compile();
 
     service = module.get<BooksService>(BooksService);
